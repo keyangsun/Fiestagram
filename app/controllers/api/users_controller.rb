022150e3b7a3f3a8cc271a409bffb3893744d000
keyangsun <keyangsun@berkeley.edu>
2019-05-09 20:06:46 -0700

    Fixed User Auth, add new.json.jbuilder

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,7 +4,7 @@ class Api::UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             login!(@user)
-            render :show 
+            render :new, status: 200
         else 
             render json: @user.errors.full_messages, status: 422
         end 
