f3cac6b105c6d8fd99d876de781f1bf70d861be6
keyangsun <keyangsun@berkeley.edu>
2019-06-06 10:20:42 -0700

    Complete Likes feature w/o styling

--- frontend/components/likes/like_bar.jsx
+++ frontend/components/likes/like_bar.jsx
@@ -3,24 +3,44 @@ import React from 'react';
 class LikeBar extends React.Component {
     constructor(props) {
         super(props);
-        this.renderheart = this.renderheart.bind(this);
+        this.renderHeart = this.renderHeart.bind(this);
+        this.createLike = this.createLike.bind(this);
+        this.removeLike = this.removeLike.bind(this); 
     }
 
-    renderheart() {
+    createLike() {
+        this.props.createLike({
+            post_id: this.props.postId 
+        });
+    }
+
+    removeLike() {
+        let { likes, currUser, removeLike } = this.props; 
+        
+        for(let i = 0; i < likes.length; i ++) {
+            if ( likes[i].user_id === currUser ) {
+                removeLike(likes[i]);
+                return; 
+            }
+        }
+    }
+
+    renderHeart() {
         let { likers, currUser } = this.props; 
         return likers.includes(currUser) ? (
-            <p>liked</p>
+            <p onClick={this.removeLike}>liked</p>
         ) : (
-            <p>not liked</p>
+            <p onClick={this.createLike}>not liked</p>
         ); 
     }
 
     render() {
+        console.log(this.props);
         return(
             <>
             <p>I'm the like bar</p>
-            {this.renderheart()}
-            <p>{this.props.likers.length}</p>
+            {this.renderHeart()}
+            <p>{this.props.likes.length}</p>
             </>
         );
     }
--- frontend/components/likes/like_bar_container.js
+++ frontend/components/likes/like_bar_container.js
@@ -3,8 +3,12 @@ import LikeBar from './like_bar';
 import { createLike, removeLike} from '../../actions/like_actions'; 
 
 const mapSTP = (state, ownProps) => {
+    let postLikes = state.entities.posts[ownProps.postId].like_ids; 
+    let filteredLikes = Object.values(state.entities.likes).filter( like => 
+        postLikes.includes(like.id));
+
     return({
-        likes: state.entities.posts[ownProps.postId].like_ids, 
+        likes: filteredLikes, 
         likers: state.entities.posts[ownProps.postId].liker_ids,
         currUser: state.session.currentUserId
     });
