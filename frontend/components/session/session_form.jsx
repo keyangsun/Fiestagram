1d90f74c92c701f11ac495aa37d4a107e8e7abdd
keyangsun <keyangsun@berkeley.edu>
2019-06-11 10:41:01 -0700

    Update navbar styling

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -132,6 +132,19 @@ class SessionForm extends React.Component {
                         {buttonmsg}
                     </p>
                 </div>
+
+                <div className="session-msg">
+                    <p>Get the real app.</p>
+                </div>
+
+                <div className="session-real-app">
+                    <a href="https://itunes.apple.com/app/instagram/id389801252?mt=8&vt=lo">
+                        <img src="/images/appstore_1.png" alt=""/>
+                    </a>
+                    <a href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3DXPxlUwAEAAE7BYcROqbd5bl-TwJy%26utm_content%3Dlo%26utm_medium%3Dbadge">
+                        <img src="/images/appstore_2.png" alt=""/>
+                    </a>
+                </div>
             </div>
         );
     }
